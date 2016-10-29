395317df60198876e8dc506a7826d1e860ef7492
jakeiwen <iwenx006@umn.edu>
2016-10-28 19:20:20 -0500

    Challenge; easy mode

--- forms/forms.js
+++ forms/forms.js
@@ -1,16 +1,19 @@
+
+
 $(document).ready(function() {
     var array = [];
-    $('#employeeinfo').on('submit', function(event) {
+    var totalSalaries = 0;
+    $('#einfo').on('submit', function(event) {
       event.preventDefault();
 
       // initialize a new variable as an empty object
       var values = {};
 
       // convert the form inputs into an array
-      var fields = $('#employeeinfo').serializeArray();
-
+      var fields = $('#einfo').serializeArray();
+      console.log(fields);
       // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
-      fields.forEach(function(element, index, array) {
+      fields.forEach(function(element) {
         // review index notation vs. dot notation on objects
         // here, dot notation wouldn't work
         values[element.name] = element.value;
@@ -19,17 +22,26 @@ $(document).ready(function() {
       console.log(values);
 
       // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
+      $('#einfo').find('input[type=text]').val('');
 
       // append to DOM
-      appendDom(values);
+      totalSalaries += parseInt(values.eSalary);
+      appendDom(values, totalSalaries);
+      console.log(totalSalaries);
     });
 
-    function appendDom(empInfo) {
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
+    function appendDom(empInfo, salaryTot) {
+      $('#tableInfo').append('<tr></tr>');
+      var $el = $('#tableInfo').children().last();
+      $el.append('<td>' + empInfo.eFirstName + '</td>');
+      $el.append('<td>' + empInfo.eLastName + '</td>');
+      $el.append('<td>' + empInfo.eId + '</td>');
+      $el.append('<td>' + empInfo.eJob + '</td>');
+      $el.append('<td>' + empInfo.eSalary + '</td>');
 
-      $el.append('<p>' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + '</p>');
+      $('#monthlySalary').remove();
+      $('#salaryTitle').append('<span id="monthlySalary">' + ' $' + salaryTot/12 + '</span>');
+
     }
 
 
