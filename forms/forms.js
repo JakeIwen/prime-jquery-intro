d1ee8f65a0b66ebc2f7f8d34bfbbd2a6238fcdcc
jakeiwen <iwenx006@umn.edu>
2016-10-30 22:41:14 -0500

    pro mode activated

--- forms/forms.js
+++ forms/forms.js
@@ -1,48 +1,62 @@
 
 
 $(document).ready(function() {
-    var array = [];
-    var totalSalaries = 0;
+    var tableRow = 0;
+    var salaryArray = [];
+    //listener for delete button
+    $('#tableInfo').on('click', "#deleteEntry", removeFromDom);
+
     $('#einfo').on('submit', function(event) {
       event.preventDefault();
-
       // initialize a new variable as an empty object
       var values = {};
-
       // convert the form inputs into an array
       var fields = $('#einfo').serializeArray();
-      console.log(fields);
       // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
       fields.forEach(function(element) {
-        // review index notation vs. dot notation on objects
-        // here, dot notation wouldn't work
         values[element.name] = element.value;
       });
+      $('#einfo').find('input[type=text]').val('');   // clear out inputs
 
-      console.log(values);
-
-      // clear out inputs
-      $('#einfo').find('input[type=text]').val('');
-
-      // append to DOM
-      totalSalaries += parseInt(values.eSalary);
-      appendDom(values, totalSalaries);
-      console.log(totalSalaries);
+      tableRow++;
+      salaryArray[tableRow] = parseInt(values.eSalary); //add salary to array
+      appendDom(values);
     });
 
-    function appendDom(empInfo, salaryTot) {
-      $('#tableInfo').append('<tr></tr>');
-      var $el = $('#tableInfo').children().last();
-      $el.append('<td>' + empInfo.eFirstName + '</td>');
-      $el.append('<td>' + empInfo.eLastName + '</td>');
-      $el.append('<td>' + empInfo.eId + '</td>');
-      $el.append('<td>' + empInfo.eJob + '</td>');
-      $el.append('<td>' + empInfo.eSalary + '</td>');
-
-      $('#monthlySalary').remove();
-      $('#salaryTitle').append('<span id="monthlySalary">' + ' $' + salaryTot/12 + '</span>');
-
+    //remove the table row containing the unique class of the clicked button
+    function removeFromDom() {
+      var thisClass = $(this).attr("class");
+      $('tr.' + thisClass).remove();
+      //get index number of salary array from digits of class
+      thisClass = thisClass.replace( /^\D+/g, ''); //remove all non-digits from classname
+      console.log(thisClass);
+      var salIndex = parseInt(thisClass);
+      //erase salary value of deleted employee from array
+      salaryArray[salIndex] = 0;
+      writeMonthlySalary();
     }
 
+    function appendDom(empInfo) {
+      //add new row to table with new employee data
+      //provide unique row class for each table row
+      var $el = $('#tableInfo').children().last();
+      $el.append(
+      '<tr class="row' + tableRow + '"><td>' + empInfo.eFirstName + '</td>' +
+      '<td>' + empInfo.eLastName + '</td>' +
+      '<td>' + empInfo.eId + '</td>' +
+      '<td>' + empInfo.eJob + '</td>' +
+      '<td>' + empInfo.eSalary + '</td>' +
+      '<td><button id="deleteEntry" class="row' + tableRow + '">Delete</button></td></tr>');
+      writeMonthlySalary();
+    }
+
+    function writeMonthlySalary() {
+      //quirky mini function that adds up all the values in the array
+      var salarySum = salaryArray.reduce(function(a, b) {
+        return a + b;
+      }, 0);
+      $('#monthlySalary').remove();
+      $('#salaryTitle').append('<span id="monthlySalary">' + ' $' + salarySum/12 + '</span>');
+    }
 
 });
